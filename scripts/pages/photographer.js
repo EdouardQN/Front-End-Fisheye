4d6f9e9963b9159b4ba350592be4c92d3502c1e3
Edouard Quennesson <edouardquennesson@macbook-air-de-edouard.home>
2023-05-12 14:46:18 +0200

    Much proper code, factory functions and media objects created, displays media titles in gray box instead of the real media, using grid in css to display that data.

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,24 +1,18 @@
 //code JavaScript lié à la page photographer.html
 
-function getPhotographerId(){
-    return new URL(location.href).searchParams.get("id");
-}
+//récupération de l'id du photographe passé en url 
+function getPhotographerId(){return new URL(location.href).searchParams.get("id");}
 
 async function getPhotographer(){
-    
-    let photographers = 
-        fetch('../../data/photographers.json')
+    let photographers = fetch('../../data/photographers.json')
         .then(response => response.json())
-        .catch(error => console.error(error))
-        ;
+        .catch(error => console.error(error));
         return photographers;
 }
 
 function getPhotographerContent(json){
     let contenuPhotographer = []; 
-    //boucle les photographes pour trouver l'id
     for (let i =0; i<json.photographers.length; i++){
-        //pas de stricte égalité car le type n'est pas le même
         if (json.photographers[i].id == photographerId){
             contenuPhotographer.push(photographerId);
             contenuPhotographer.push(json.photographers[i].name);
@@ -35,9 +29,7 @@ function getPhotographerContent(json){
 function getPhotographerMedia(json){
     let x = 0;
     let contenuMedia = [];
-    //boucle les datas img etc lorsque l'id du photographe correspond
     for (let j = 0; j<json.media.length; j++){
-
         if (json.media[j].photographerId == photographerId){
             contenuMedia[x] = json.media[j];
             x++;
@@ -46,10 +38,7 @@ function getPhotographerMedia(json){
     return contenuMedia;
 }
 
-
 function photographerFactory(content){
-
-    //details du photographe
     const id = content[0];
     const name = content[1];   
     const city = content[2];  
@@ -60,31 +49,26 @@ function photographerFactory(content){
     const picture = `../../assets/photographers/${portrait}`;
 
     function getPhotographerDomContent(){
-
         const div = document.createElement('div');
-        div.classList.add('photograph-content');
-
         const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-
         const h4 = document.createElement('h4');
-        h4.textContent = city + ', ' + country;
-
         const span = document.createElement('span');
+
+        div.classList.add('photograph-content');
+        h2.textContent = name;
+        h4.textContent = city + ', ' + country;
         span.textContent = tagline;
 
         div.appendChild(h2);
         div.appendChild(h4);
         div.appendChild(span);
-
         return (div);
     }
     function getPhotographerPicture(){
-
         const divImg = document.createElement('div');
-        divImg.classList.add('photograph-img');
-
         const img = document.createElement( 'img' );
+
+        divImg.classList.add('photograph-img');
         img.setAttribute("src", picture);
 
         divImg.appendChild(img);
@@ -92,46 +76,100 @@ function photographerFactory(content){
         return (divImg);
     }
 
-    return { id, name, city, country, tagline, price, portrait, picture, getPhotographerDomContent, getPhotographerPicture }
+    function getPhotographerTarif(){
+        const tarif = document.querySelector('.photograph-tarif');
+        tarif.innerText = `${price}€ / jour`;
+        return tarif;
+    }
+
+    return { id, name, city, country, tagline, price, portrait, picture, getPhotographerDomContent, getPhotographerPicture, getPhotographerTarif }
+}
+
+var Factory = function(){
+    this.createMedia = function (json){
+        var createMedia;
+        if (json.hasOwnProperty('image')){
+            createMedia = new Image(json.image);
+        }
+        else if (json.hasOwnProperty('video')){
+            createMedia = new Video(json.video);
+        }
+        createMedia.date = json.date;
+        createMedia.likes = json.likes;
+        createMedia.photographerid = json.photographerId;
+        createMedia.price = json.price;
+        createMedia.title = json.title;
+        return createMedia;
+    }
+}
+
+var Image = function(type){
+    this.image = type;
+}
+
+var Video = function(type){
+    this.video = type;
 }
 
 function mediaFactory(media){
-      
-    //condition pour voir si c'est une image ou une vidéo à faire  if hasOwnProperty() image --> new Image, else new Video
-    console.log(media);
-    const photo = media[0].image;
-    const title = media[0].title
-    console.log(title);
+    var tabMedia= [];
+    var contentMedia;
+    // const sample = `../../assets/samples/${}`;
+    var nbLikes = 0;
+    var factory = new Factory();
 
-    function getPhotographerMedia(){
-
+    function getPhotographerDomMedia(){
+        let div = [];
+        for (let i = 0; i<media.length; i++){
+            div[i] = document.createElement('div');
+            tabMedia.push(factory.createMedia(media[i]));
+            if (tabMedia[i].hasOwnProperty('image')){
+                contentMedia = tabMedia[i].image;
+                div[i].classList.add('photograph-image');
+            }
+            else if (tabMedia[i].hasOwnProperty('video')){
+                contentMedia = tabMedia[i].video;
+                div[i].classList.add('photograph-video');
+            }            
+            div[i].innerText = contentMedia;
+            nbLikes += media[i].likes;
+        }
+        return (div);
     }
 
+    function getPhotographerLikes(){
+        const likes = document.querySelector('.photograph-likes');
+        likes.innerText = nbLikes;
+        return likes;
+    }
+    return {tabMedia, nbLikes, factory, getPhotographerDomMedia, getPhotographerLikes};
 }
-
-
 
 const photographerId = getPhotographerId();
 const header = document.querySelector('.photograph-header');
 const galery = document.querySelector('.photograph-galery');
 
 async function init(){
+    //fetch
     const data = await getPhotographer();
+    //Récupération données photographe / ses médias, séparés en 2 tableaux d'objects
     const photographerContent = getPhotographerContent(data);
     const photographerMedia = getPhotographerMedia(data);
-
+    //factory functions qui utilisent leur tableau correspondant au dessus
     const photographerModel = photographerFactory(photographerContent);
     const mediaModel = mediaFactory(photographerMedia);
-
+    //DOM Maniplation
     const userCardDOM = photographerModel.getPhotographerDomContent();
     const pictureCardDom = photographerModel.getPhotographerPicture();
-    // const mediaCardDom = mediaModel.getPhotographerMedia();
-
+    const tarifCardDom = photographerModel.getPhotographerTarif();
+    const mediaCardDom = mediaModel.getPhotographerDomMedia();
+    const likesDom = mediaModel.getPhotographerLikes();
     header.appendChild(userCardDOM);
     header.appendChild(pictureCardDom);
+    for (let i=0; i<mediaCardDom.length; i++){
 
-    // galery.appendChild(mediaCardDom);
-
+        galery.append(mediaCardDom[i]);
+    }
 }
 
 init();
