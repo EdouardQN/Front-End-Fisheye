759f71081ab9bdb78f5229896addf01c69a52d52
Edouard Quennesson <edouardquennesson@macbook-air-de-edouard.home>
2023-05-22 14:26:52 +0200

    Updated contact form. Added accecibility to it, inputs we needed and updated the container to a dialog modal concept, with console logs for the values, then it reset as soon as we press the button. Only need to display the name of the photographer in the form's title.

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,56 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+const modal = document.querySelector('.modal');
+const form = document.querySelector('.contactForm');
+const openModal = document.querySelector('.contact_button');
+const closeModal = document.querySelector('.close_modal');
+const submitModal = document.querySelector('.submit_button');
+
+const prenomForm = document.querySelector('#prenom');
+const nomForm = document.querySelector('#nom');
+const emailForm = document.querySelector('#email');
+const messageForm = document.querySelector('#message');
+
+openModal.addEventListener("click", () => {
+    modal.showModal();
+    modal.style.visibility = "visible";
+})
+
+closeModal.addEventListener("click", () => {
+    modal.close();
+    modal.style.visibility = "hidden";
+})
+
+function sendData(){
+    event.preventDefault();
+    modal.close();
+    modal.style.visibility = "hidden";
+    console.log("Envoie des données : ");
+    console.log("prenom : ", prenomForm.value);
+    console.log("nom : ", nomForm.value);
+    console.log("email : ", emailForm.value);
+    console.log("message : ", messageForm.value);
+    form.reset();
+    
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+
+async function getPhotographer(){
+    let data = fetch('../../data/photographers.json')
+        .then(response => response.json())
+        .catch(error => console.error(error));
+        return data;
 }
+
+function displayPhotographerName(data){
+    for (let i=0; i< data.length; i++){
+        console.log(data);
+    }
+    return data;
+}
+
+async function display(){
+    const dataPhotographers = await getPhotographer();
+    let photographName = displayPhotographerName(dataPhotographers);
+}
+
+display();
+
